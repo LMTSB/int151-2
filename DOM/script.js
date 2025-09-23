587902b4de938f3a0a5557d0d37dee78c3408c4c
pattui <pattyzul@users.noreply.github.com.>
2025-09-23 17:03:11 +0700

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -6,15 +6,15 @@ const lastElementChildNode = document.lastElementChild
 const divElement = document.getElementById('exampleElement')
 
 //div child
-const divChild = divElement.childNodes
+//const divChild = divElement.childNodes
 //divChild.forEach(child => {console.log(child.nodeName),console.log(child.nodeType),console.log(child.nodeValue)});
 
 //div children
-const divChildren = divElement.children
+//const divChildren = divElement.children
 //Array.from(divChildren).forEach(children => {console.log(children.nodeName),console.log(children.nodeType),console.log(children.nodeValue)});
 
 //create element
-const id = document.getElementById('subject');
+
 
 const p = document.createElement("p");
 
@@ -25,7 +25,7 @@ p.setAttribute("name","core");
 // modify text node
 p.textContent = "INT141";
 // add element
-id.appendChild(p);
+//id.appendChild(p);
 
 // get attribute value
 const pAttributes = p.attributes;
@@ -45,3 +45,19 @@ for (let i= 0; i < pAttributes.length ; i++) {
 //document.body.nodeType === Node.ELEMENT_NODE ? alert ("Node is an element"): alert ("Node is not an element")
 //document.body.nodeType === Node.TEXT_NODE ? alert ("Node is text"): alert ("Node is not a text")
 //document.body.nodeType === Node.ATTRIBUTE_NODE ? alert ("Node is an attribute"): alert ("Node is not an attribute")
+
+
+
+const drinkMenu = document.querySelector('#drink');
+const firstDrinkNode = drinkMenu.firstElementChild //first child
+const nextFirstChildNode = firstElementChildNode.nextElementSibling //next sibling
+const lastChildNode = drinkMenu.lastElementChild // last child
+const previousLastChildNode = lastChildNode.previousElementSibling // previous child
+
+console.log(Array.from(drinkMenu).map(drinkMenu.TEXT_NODE === "Tea"));
+
+console.log(drinkMenu.textContent);
+console.log(firstDrinkNode);
+console.log(nextFirstChildNode);
+console.log(lastChildNode);
+console.log(previousLastChildNode);
