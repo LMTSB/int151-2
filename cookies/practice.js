7691ddd066f391413e4aa32a2a7918c2923f4113
pattui <pattyzul@users.noreply.github.com.>
2025-10-14 16:54:25 +0700

    week10

---
+++ cookies/practice.js
@@ -0,0 +1,16 @@
+let save = document.getElementById("save"),
+    reset = document.getElementById("reset"),
+    background = document.getElementById("Background").value,
+    color = document.getElementById("Color").value,
+    size = document.getElementById("Size").value;
+
+background.addEventListener("input", () => {})
+
+save.addEventListener("click", () => {
+    
+    console.log(background);
+});
+
+reset.addEventListener("click", () => {
+
+});
