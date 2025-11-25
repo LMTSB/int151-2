acaab7c60e3c1a7f42a4e053b5846750da8df968
Student <Student@Train11-4-PC055.control.ad.sit.kmutt.ac.th>
2025-11-25 15:16:15 +0700

    add displayDialog function in dialogTest.js

--- dialogPractice/dialogTest.js
+++ dialogPractice/dialogTest.js
@@ -1,18 +1,31 @@
+// const okBtn = document.getElementById("okBtn");
+// const cancelBtn_1 = document.getElementById("cancelBtn_1");
+
+// okBtn.addEventListener("click", () => { confirmDialog.showModal(); res(okBtn.textContent) })
+// cancelBtn_1.addEventListener("click", () => { confirmDialog.showModal(); res(cancelBtn_1.textContent) })
 
 
-const displayDialog = async () => {
+const confirmDialog = document.getElementById("confirmDialog");
+
+const openDialogBtn = document.getElementById("openDialogBtn");
+
+openDialogBtn.addEventListener("click", async () => { 
+    const result = await displayDialog();
+
+})
+
+const displayDialog = async () => { // make a button the show a dialog
     return new Promise((resolve) => {
-        const messageDialog = document.getElementById("messageDialog") ;
-            
+        const messageDialog = document.getElementById("messageDialog");
+
         const confirmBtn = document.getElementById("confirmBtn");
         const cancelBtn = document.getElementById("cancelBtn");
-        
-        confirmBtn.addEventListener("click", () => {messageDialog.close(); resolve(confirmBtn.textContent)})
-        cancelBtn.addEventListener("click", () => {messageDialog.close();  resolve(cancelBtn.textContent)})
 
-        messageDialog.showModal()
+        confirmBtn.addEventListener("click", () => { messageDialog.close(); resolve(confirmBtn.textContent) })
+        cancelBtn.addEventListener("click", () => { messageDialog.close(); resolve(cancelBtn.textContent) })
+
+        return messageDialog.showModal()
     })
 };
 
-const result = await displayDialog();
-console.log(result)
+
